87ecd3985f6d7822fda654eb7e12a99cfdc10084
motati <72252670+motati@users.noreply.github.com>
2021-07-09 15:32:57 +0530

    Update urls.py

--- JeeSeatAllocation/urls.py
+++ JeeSeatAllocation/urls.py
@@ -1,12 +1,18 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from Candidate import views
 
-app_name = 'Candidate'
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('candidate/', include('Candidate.urls', namespace="Candidate")),
     path('', views.home, name='home'),
-    path('choicefilling/', views.ChoiceFilling, name="choicefilling"),
-    path('user_login/', views.user_login, name='user_login'),
-    path('change_password/', views.change_password, name='change_password'),
-    path('register/', views.register, name='register'),
-
+    path('logout/', views.user_logout, name='logout'),
+    path('assign/', views.assign, name='assign'),
+    path('tozero/', views.brnull, name='tozero'),
+    path('assign-admin/', views.admin_home, name='admin_home'),
+    path('tofreeze/', views.to_freeze, name='tofreeze'),
+    path('toslide/', views.to_slide, name='toslide'),
+    path('toremove/', views.to_remove, name='toremove'),
 ]
+
+
